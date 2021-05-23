020e914925febda55619772e7f2f0026cd8e0c9f
lloydnoone <lloydnoone92@gmail.com>
2021-05-23 15:23:33 +0100

    add snippet route tests

---
+++ src/itest/controllers/snippetController.test.ts
@@ -0,0 +1,34 @@
+import { request } from '../../test/helper'
+import { Response } from 'supertest';
+
+const snippetCorrect = {
+  snippet: "const Users = code"
+}
+
+const snippetIncorrect = {
+  snippet: 1
+}
+
+describe('POST /login', () => {
+  test('should return a 422 unproccessible entity response for snippet that isnt a string', async (done) => {
+    request
+      .post('/snippets')
+      .type('form')
+      .send(snippetIncorrect)
+      .end((err: Error, res: Response) => {
+        expect(res.status).toBe(422)
+        done()
+      })
+  })
+
+  test('should return a 200 ok response for a snippet that is a string', async (done) => {
+    request
+      .post('/snippets')
+      .type('form')
+      .send(snippetCorrect)
+      .end((err: Error, res: Response) => {
+        expect(res.status).toBe(302)
+        done()
+      })
+  })
+})
